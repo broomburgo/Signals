46ed889bd1e4ef7b0363a5b7764a5614d985b6d0
Elviro Rocca <elviro.rocca@facile.it>
2016-07-10 19:31:02 +0200

    Initial Commit

---
+++ Signals/Signals.h
@@ -0,0 +1,19 @@
+//
+//  Signals.h
+//  Signals
+//
+//  Created by Elviro Rocca on 10/07/16.
+//  Copyright © 2016 ExFalsoQuodlibet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Signals.
+FOUNDATION_EXPORT double SignalsVersionNumber;
+
+//! Project version string for Signals.
+FOUNDATION_EXPORT const unsigned char SignalsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Signals/PublicHeader.h>
+
+
