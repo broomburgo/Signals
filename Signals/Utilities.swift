6f09398bc9a182e82140489492044fe28a9bf9e5
Elviro Rocca <elviro.rocca@facile.it>
2017-04-18 10:22:13 +0200

    added convenience 'weakly' functions

--- Signals/Utilities.swift
+++ Signals/Utilities.swift
@@ -15,3 +15,35 @@ extension DispatchQueue {
 		}
 	}
 }
+
+public func weakly<T>(_ reference: T?, closure: @escaping (T) -> ()) -> () -> Persistence where T: AnyObject {
+	return { [weak reference] in
+		guard let some = reference else { return .stop }
+		closure(some)
+		return .again
+	}
+}
+
+public func weakly<T,I>(_ reference: T?, closure: @escaping (T,I) -> ()) -> (I) -> Persistence where T: AnyObject {
+	return { [weak reference] input in
+		guard let some = reference else { return .stop }
+		closure(some,input)
+		return .again
+	}
+}
+
+public func weakly<T,I1,I2>(_ reference: T?, closure: @escaping (T,I1,I2) -> ()) -> (I1,I2) -> Persistence where T: AnyObject {
+	return { [weak reference] (input1, input2) in
+		guard let some = reference else { return .stop }
+		closure(some,input1,input2)
+		return .again
+	}
+}
+
+public func weakly<T,I1,I2,I3>(_ reference: T?, closure: @escaping (T,I1,I2,I3) -> ()) -> (I1,I2,I3) -> Persistence where T: AnyObject {
+	return { [weak reference] (input1, input2, input3) in
+		guard let some = reference else { return .stop }
+		closure(some,input1,input2,input3)
+		return .again
+	}
+}
